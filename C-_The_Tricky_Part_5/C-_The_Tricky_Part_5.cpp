0a04a03657b5f7ee355ed38601ffab652f52973b
Morheim <90184938+Morheim@users.noreply.github.com>
2021-12-04 22:02:53 +0300

    Update C-_The_Tricky_Part_5.cpp

--- C-_The_Tricky_Part_5/C-_The_Tricky_Part_5.cpp
+++ C-_The_Tricky_Part_5/C-_The_Tricky_Part_5.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <random>
 #include <chrono>
 #include <set>
@@ -9,164 +9,182 @@
 
 using namespace std;
 
+void DZ1()
+{
+	cout << "//===================================================[DZ1]===================================================//\n\n\n";
+}
+
+void DZ2()
+{
+	cout << "\n//===================================================[DZ2]===================================================//\n\n\n";
+}
+
+void END()
+{
+	cout << "\n\n//===================================================[END]===================================================//\n\n";
+}
+
 const size_t SIZE_LIST = 11;
 
-int RandomNumber(int min, int max) 
+int RandomNumber(int min, int max)
 {
-    const static auto seed = chrono::system_clock::now().time_since_epoch().count();
-    static mt19937_64 generator(seed);
-    uniform_int_distribution<int> dis(min, max);
-    return dis(generator);
+	const static auto seed = chrono::system_clock::now().time_since_epoch().count();
+	static mt19937_64 generator(seed);
+	uniform_int_distribution<int> dis(min, max);
+	return dis(generator);
 }
 
 template<typename T>
-void PrintAll(const T& begin, const T& end) 
+void PrintAll(const T& begin, const T& end)
 {
-    for (T p1 = begin; p1 != end; p1++) 
-    {
-        cout << *p1 << '\t';
-    }
-    cout << endl << endl;
+	for (T p1 = begin; p1 != end; p1++)
+	{
+		cout << *p1 << '\t';
+	}
+	cout << endl << endl;
 }
 
 template<typename T>
-void PrintUnique(const T& begin, const T& end) 
+void PrintUnique(const T& begin, const T& end)
 {
-    for (T p1 = begin; p1 != end; p1++) 
-    {
-        T p2;
-        for (p2 = begin; p2 != p1; p2++) 
-        {
-            if (*p1 == *p2)
-                break;
-        }
-        if (p1 == p2)
-            cout << *p1 << '\t';
-    }
-    cout << endl << endl;
+	for (T p1 = begin; p1 != end; p1++)
+	{
+		T p2;
+		for (p2 = begin; p2 != p1; p2++)
+		{
+			if (*p1 == *p2)
+				break;
+		}
+		if (p1 == p2)
+			cout << *p1 << '\t';
+	}
+	cout << endl << endl;
 }
 
-string GenerateRandomString() 
+string GenerateRandomString()
 {
-    int length = RandomNumber(3, 10);
-    bool uppercase = RandomNumber(0, 1);
-    string result;
-    for (size_t i = 0; i < length; i++) 
-    {
-        if (uppercase) 
-        {
-            result += char(RandomNumber('A', 'Z'));
-        }
-        else 
-        {
-            result += char(RandomNumber('a', 'z'));
-        }
-    }
-    return result;
+	int length = RandomNumber(3, 10);
+	bool uppercase = RandomNumber(0, 1);
+	string result;
+	for (size_t i = 0; i < length; i++)
+	{
+		if (uppercase)
+		{
+			result += char(RandomNumber('A', 'Z'));
+		}
+		else
+		{
+			result += char(RandomNumber('a', 'z'));
+		}
+	}
+	return result;
 }
 
-auto cmp = [](const string& a, const string& b) 
+auto cmp = [](const string& a, const string& b)
 {
-    return a.length() > b.length(); 
+	return a.length() > b.length();
 };
 
-set<string, decltype(cmp)> SplitString(string& str, char delimiter = '.') 
+set<string, decltype(cmp)> SplitString(string& str, char delimiter = '.')
 {
-    set<string, decltype(cmp)> result;
-    auto start = 0U;
-    auto end = str.find(delimiter);
-    while (end != string::npos) 
-    {
-        result.insert(str.substr(start, end - start));
-        start = end + 1;
-        end = str.find(delimiter, start);
-    }
-    if (start != end) 
-    {
-        result.insert(str.substr(start, end - start));
-    }
-    return result;
+	set<string, decltype(cmp)> result;
+	auto start = 0U;
+	auto end = str.find(delimiter);
+	while (end != string::npos)
+	{
+		result.insert(str.substr(start, end - start));
+		start = end + 1;
+		end = str.find(delimiter, start);
+	}
+	if (start != end)
+	{
+		result.insert(str.substr(start, end - start));
+	}
+	return result;
 }
 
-void exercise1() 
+void exercise1()
 {
-    {
-        list<string> word_list;
-        word_list.push_back(GenerateRandomString());
-        for (size_t i = 1; i < SIZE_LIST; i++) 
-        {
-            bool repeat = RandomNumber(0, 1);
-            if (repeat) 
-            {
-                int repeat_index = RandomNumber(0, i - 1);
-                auto word_iterator = word_list.begin();
-                advance(word_iterator, repeat_index);
-                string repeat_word = *word_iterator;
-                word_list.push_back(repeat_word);
-            }
-            else 
-            {
-                word_list.push_back(GenerateRandomString());
-            }
-        }
-        PrintAll(word_list.begin(), word_list.end());
-        PrintUnique(word_list.begin(), word_list.end());
-    }
+	{
+		list<string> word_list;
+		word_list.push_back(GenerateRandomString());
+		for (size_t i = 1; i < SIZE_LIST; i++)
+		{
+			bool repeat = RandomNumber(0, 1);
+			if (repeat)
+			{
+				int repeat_index = RandomNumber(0, i - 1);
+				auto word_iterator = word_list.begin();
+				advance(word_iterator, repeat_index);
+				string repeat_word = *word_iterator;
+				word_list.push_back(repeat_word);
+			}
+			else
+			{
+				word_list.push_back(GenerateRandomString());
+			}
+		}
+		PrintAll(word_list.begin(), word_list.end());
+		PrintUnique(word_list.begin(), word_list.end());
+	}
 
-    {
-        multiset<string> word_list;
-        word_list.insert(GenerateRandomString());
-        for (size_t i = 1; i < SIZE_LIST; i++) 
-        {
-            bool repeat = RandomNumber(0, 1);
-            if (repeat) 
-            {
-                int repeat_index = RandomNumber(0, i - 1);
-                auto word_iterator = word_list.begin();
-                advance(word_iterator, repeat_index);
-                string repeat_word = *word_iterator;
-                word_list.insert(repeat_word);
-            }
-            else 
-            {
-                word_list.insert(GenerateRandomString());
-            }
-        }
-        PrintAll(word_list.begin(), word_list.end());
-        PrintUnique(word_list.begin(), word_list.end());
-    }
+	{
+		multiset<string> word_list;
+		word_list.insert(GenerateRandomString());
+		for (size_t i = 1; i < SIZE_LIST; i++)
+		{
+			bool repeat = RandomNumber(0, 1);
+			if (repeat)
+			{
+				int repeat_index = RandomNumber(0, i - 1);
+				auto word_iterator = word_list.begin();
+				advance(word_iterator, repeat_index);
+				string repeat_word = *word_iterator;
+				word_list.insert(repeat_word);
+			}
+			else
+			{
+				word_list.insert(GenerateRandomString());
+			}
+		}
+		PrintAll(word_list.begin(), word_list.end());
+		PrintUnique(word_list.begin(), word_list.end());
+	}
 }
 
-void exercise2() 
+void exercise2()
 {
-    string sentences;
-    cout << "Enter some string: ";
-    getline(cin, sentences);
-    cout << sentences << endl;
-    set<string, decltype(cmp)> container = SplitString(sentences);
-    copy(container.begin(), container.end(), ostream_iterator<string>(cout, " \n"));
+	string sentences;
+	cout << "Enter some string: ";
+	getline(cin, sentences);
+	cout << sentences << endl;
+	set<string, decltype(cmp)> container = SplitString(sentences);
+	copy(container.begin(), container.end(), ostream_iterator<string>(cout, " \n"));
 }
 
-int main() 
+int main()
 {
-    {
-    /*     DZ1
-        Создать шаблонную функцию, которая принимает итераторы на начало и конец последовательности слов,
-    и выводящую в консоль список уникальных слов
-    (если слово повторяется больше 1 раза, то вывести его надо один раз).
+	{
+		/*     DZ1
+			Создать шаблонную функцию, которая принимает итераторы на начало и конец последовательности слов,
+		и выводящую в консоль список уникальных слов
+		(если слово повторяется больше 1 раза, то вывести его надо один раз).
 
-        Продемонстрировать работу функции, передав итераторы различных типов.
-    */
-    exercise1();
-    }
+			Продемонстрировать работу функции, передав итераторы различных типов.
+		*/
+		DZ1();
+		exercise1();
+	}
 
-    {
-    /*      DZ2
-    	Используя ассоциативный контейнер, напишите программу,
-    которая будет считывать данные введенные пользователем из стандартного потока ввода и разбивать их на предложения.
+	{
+		/*      DZ2
+			Используя ассоциативный контейнер, напишите программу,
+		которая будет считывать данные введенные пользователем из стандартного потока ввода и разбивать их на предложения.
 
-    	Далее программа должна вывести пользователю все предложения, отсортировав их по длине.
-    */
-    exercise2();
-    }
+			Далее программа должна вывести пользователю все предложения, отсортировав их по длине.
+		*/
+		DZ2();
+		exercise2();
+	}
+	END();
 }
